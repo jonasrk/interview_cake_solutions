bbfca41c9a63c10644c52a71cf617acf8756f283
Jonas <jonas.kemper.biz@gmail.com>
2018-01-08 13:31:54 +0100

    Q13 O(lg n)

--- Q13-find-rotation-point/find-rotation-point.py
+++ Q13-find-rotation-point/find-rotation-point.py
@@ -1,21 +1,23 @@
-def sort_list(list):
-    rot_word = sorted(list)[0]
-    return find_rot_word(list, rot_word)
-
-def find_rot_word(list, word):
+def find_rot_word(list):
     index = len(list) // 2
     step = len(list) // 2
     found = False
     while not found:
-        if list[index]:
+        if is_rot_point(list, index):
             return index
-        elif list[index] < word:
+        elif list[index] < list[0]:
             index = (index - step) % len(list)
             step = step // 2
 
-        elif list[index] > word:
+        elif list[index] > list[0]:
             index = (index + step) % len(list)
             step = step // 2
+
+def is_rot_point(list, index):
+    if list[(index-1)%len(list)] > list[index]:
+        return True
+    else:
+        return False
 
 
 words = [
@@ -32,4 +34,4 @@ words = [
     'othellolagkage',
 ]
 
-print(sort_list(words))
+print(find_rot_word(words))
