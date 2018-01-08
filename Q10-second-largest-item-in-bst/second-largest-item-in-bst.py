6fcc667329842e43df3b8189ab288fd11f40d553
Jonas <jonas.kemper.biz@gmail.com>
2018-01-08 12:36:59 +0100

    Q10 O(1) space

--- Q10-second-largest-item-in-bst/second-largest-item-in-bst.py
+++ Q10-second-largest-item-in-bst/second-largest-item-in-bst.py
@@ -15,23 +15,39 @@ class BinaryTreeNode:
         return self.right
 
     def get_second_largest(self):
-        if self.right:
-            return self.right.get_second_largest()
-        if self.left:
-            return self.left.get_first_largest()
-        elif self.parent:
-            return self.parent.value
+        current_node = self
+        at_largest = False
+        while not at_largest:
+            if current_node.right:
+                current_node = current_node.right
+            elif current_node.parent:
+                at_largest = True
+                current_node = current_node.parent
+            elif current_node.left:
+                return current_node.left.get_first_largest()
+            else:
+                raise ValueError("Only one element")
+
+        if current_node.left:
+            return current_node.left.get_first_largest()
         else:
-            raise ValueError("Only one element")
+            return current_node.value
 
     def get_first_largest(self):
-        if self.right:
-            return self.right.get_first_largest()
-        else:
-            return self.value
+        current_node = self
+        at_largest = False
+        while not at_largest:
+            if current_node.right:
+                current_node = current_node.right
+            else:
+                return current_node.value
 
 A = BinaryTreeNode(10, None)
 A.insert_left(5, A)
 A.left.insert_right(8, A.left)
+
+A.insert_right(15, A)
+A.right.insert_right(20, A.right)
+A.right.right.insert_right(25, A.right.right)
 
 print(A.get_second_largest())
