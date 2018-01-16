336774cc1cad795e96e61a394c4ad80d8d11cf54
Jonas <jonas.kemper.biz@gmail.com>
2018-01-16 11:23:06 +0100

    Q23-linked-list-cycles complexity: O(n) space / O(1) space

--- Q23-linked-list-cycles/linked-list-cycles.py
+++ Q23-linked-list-cycles/linked-list-cycles.py
@@ -1,26 +1,31 @@
+import sys
+
 class LinkedListNode:
     def __init__(self, value):
         self.value = value
         self.next  = None
 
 def contains_cycle(first_node):
-    s = set()
+    max = -sys.maxsize
     current_node = first_node
 
     while True:
         if not current_node.next:
             return False
-        elif current_node.next.value in s:
+        elif current_node.next.value == max:
             return True
         else:
-            s.add(current_node.next.value)
+            if current_node.next.value > max:
+                max = current_node.next.value
             current_node = current_node.next
 
 
-a = LinkedListNode('A')
-b = LinkedListNode('B')
+a = LinkedListNode(1)
+b = LinkedListNode(3)
 
 a.next = b
 # b.next = a
 
 print(contains_cycle(a))
+
+# complexity: O(n) space / O(n) space
