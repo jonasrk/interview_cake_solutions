e2240ec356ca9e1897cd38642a02f1a9b1d91441
Jonas <jonas.kemper.biz@gmail.com>
2018-01-17 10:47:12 +0100

    Q33-which-appears-twice init

---
+++ Q33-which-appears-twice/which-appears-twice.py
@@ -0,0 +1,19 @@
+def appears_twice(input):
+    n = len(input) - 1
+    sum = None
+    if n % 2 == 0:
+        sum = (n / 2) * (input[0] + input[-1])
+    else:
+        sum = ((n - 1) / 2) * (input[0] + input[-1]) + input[(len(input)) - 1 / 2]
+
+    in_sum = 0
+
+    for el in input:
+        in_sum += el
+
+    return in_sum - sum
+
+input = [1, 2, 3, 2, 4]
+
+# should return 2
+print(appears_twice(input))
