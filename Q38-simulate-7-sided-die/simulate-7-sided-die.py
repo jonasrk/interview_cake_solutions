f828e0e33af51d00a7afefdca1f88e36e052462a
Jonas <jonas.kemper.biz@gmail.com>
2018-01-17 13:41:51 +0100

    Q38-simulate-7-sided-die init

--- Q38-simulate-7-sided-die/simulate-7-sided-die.py
+++ Q38-simulate-7-sided-die/simulate-7-sided-die.py
@@ -3,23 +3,16 @@ import random
 def rand5():
     return random.randint(1,5)
 
-def rand01():
-    res = 5
-    while res > 2:
-        res = rand5()
-    return res - 1
-
 def rand7():
     result = None
     found = False
     while not found:
-        b0 = rand01()
-        b1 = rand01()
-        b2 = rand01()
+        b0 = rand5()
+        b1 = rand5()
 
-        dec = b0 + 2 * b1 + 4 * b2
+        dec = b0 + 5 * b1
 
-        if dec > 0:
+        if dec >= 1 and dec <= 7:
             result = dec
             found = True
 
