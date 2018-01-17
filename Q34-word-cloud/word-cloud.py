86dc9992b9e5e019df8fad33e33e1c3890fe22b3
Jonas <jonas.kemper.biz@gmail.com>
2018-01-17 11:03:33 +0100

    Q34-word-cloud can handle hyphenated words

--- Q34-word-cloud/word-cloud.py
+++ Q34-word-cloud/word-cloud.py
@@ -1,7 +1,7 @@
 def count_words(input):
     result = {}
     for word in input.split(' '):
-        l_word = ''.join(e for e in word.lower() if e.isalnum()) 
+        l_word = ''.join(e for e in word.lower() if (e.isalnum() or e == '-')) 
         if l_word in result:
             result[l_word] += 1
         else:
@@ -10,6 +10,6 @@ def count_words(input):
     return result
 
 s = '''After beating the eggs, Dana read the next step: 
-Add milk and eggs, then add flour and sugar.'''
+Add milk and eggs, then add flour and sugar-bags.'''
 
 print(count_words(s))
