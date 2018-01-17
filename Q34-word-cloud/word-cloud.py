515167c9db1cfabc587dc55d53e66e72d8270435
Jonas <jonas.kemper.biz@gmail.com>
2018-01-17 11:08:41 +0100

    Q34-word-cloud only make first word lower case

--- Q34-word-cloud/word-cloud.py
+++ Q34-word-cloud/word-cloud.py
@@ -1,7 +1,8 @@
 def count_words(input, result):
-    result = {}d
-    for word in input.split(' '):
-        l_word = ''.join(e for e in word.lower() if (e.isalnum() or e == '-')) 
+    for i, word in enumerate(input.split(' ')):
+        l_word = ''.join(e for e in word if (e.isalnum() or e == '-')) 
+        if i == 0:
+            l_word = l_word.lower()
         if l_word in result:
             result[l_word] += 1
         else:
